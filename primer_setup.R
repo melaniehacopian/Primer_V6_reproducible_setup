0f5e20ea1895d716f8e94199a3d048f8a1a77183
melaniehacopian <hacopiam@uci.edu>
2023-06-15 12:17:50 -0700

    added readme and primer template

---
+++ primer_setup.R
@@ -0,0 +1,27 @@
+## Primer 6 set up 
+## create distance matrix from your OTU table 
+set.seed(999)
+bray.dist.matrix <- avgdist(OTU_TABLE_WIDE, sample = (# you want to rarefy to), meanfun = median,transf= sqrt, iterations = 100, round.out = T)
+
+# create tsv
+write.table((as.data.frame(as.matrix(bray.dist.matrix))), file = "bray_matrix.tsv", quote=FALSE, sep='\t', col.names = NA)
+
+## Get matching metadata 
+
+metadata <- YOUR_METADATA_DF %>%
+  rownames_to_column("sample_identifier") 
+
+sample_w_otus <- row.names(bray.dist.matrix)
+
+# filter metadata based on rarefied OTU table
+matching_metadata <- metadata %>%
+  filter(sample_identifier %in% sample_w_otus)
+
+matching_metadata <- matching_metadata %>%
+  column_to_rownames("sample_identifier") 
+
+# create tsv
+write.table(t(matching_metadata), file = "matching_metadata_bray.tsv", quote=FALSE, sep='\t', col.names = NA)
+
+
+#### Open both tsvs in excel. Copy the metadata and paste it under the matrix with one row in between. Save as a new tsv and import into Primer as "aggregate data". 
---
+++ readme.md
@@ -0,0 +1,2 @@
+This R script template will generate a bray dissimilarity matrix and matching metadata from your OTU table, which you can then import into the Primer V6 program to run PERMANOVAs or other analyses. 
+  
